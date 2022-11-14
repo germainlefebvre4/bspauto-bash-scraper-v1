50720a4f5c83d462e6c67eaa4980b0856de951d1
Germain Lefebvre <germain.lefebvre4@gmail.com>
2022-11-15 00:25:16 +0100

    feat(bspauto): Add CLI parameters

--- bsp-auto.sh
+++ bsp-auto.sh
@@ -1,30 +1,74 @@
 #!/bin/bash
 #set -x
-date=`date "+%F %T"`
-dirname=$(dirname `readlink -f $0`)
-dirname='.'
 
-trip_date_from="14/05/2023"
-trip_date_to="24/05/2023"
-trip_date_from_encoded=$(echo -n ${trip_date_from} | jq -sRr @uri)
-trip_date_to_encoded=$(echo -n ${trip_date_to} | jq -sRr @uri)
+QUIET=1
 
-header="date"
-content=""
+TRIP_DATE_FROM="14/05/2023"
+TRIP_DATE_TO="24/05/2023"
+
 cars=()
 cars[0]="RENAULT MEGANE"
 cars[1]="RENAULT SCENIC"
 cars[2]="PEUGEOT 3008"
 cars[3]="JEEP COMPASS"
 cars[4]="VW PASSAT"
-current_price=257
+CURRENT_PRICE=257
 
-URL='https://www.bsp-auto.com/fr/list.asp?ag_depart=2715&date_a='${trip_date_from_encoded}'&heure_a=15%3A00&ag_retour=2715&date_d='${trip_date_to_encoded}'&heure_d=15%3A00&chkage=1&age=25'
+POSITIONAL_ARGS=()
 
-echo "From ${trip_date_from} to ${trip_date_to}"
-echo "URL: ${URL}"
-echo "Result file: $dirname/bsp-auto"
-echo ""
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -q|--quiet)
+      QUIET=0
+      shift # past argument
+      ;;
+    -df|--date-to)
+      TRIP_DATE_FROM="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -dt|--date-to)
+      TRIP_DATE_TO="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -p|--current-price)
+      CURRENT_PRICE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+date=`date "+%F %T"`
+dirname=$(dirname `readlink -f $0`)
+dirname='.'
+
+TRIP_DATE_FROM_ENCODED=$(echo -n ${TRIP_DATE_FROM} | jq -sRr @uri)
+TRIP_DATE_TO_ENCODED=$(echo -n ${TRIP_DATE_TO} | jq -sRr @uri)
+
+header="date"
+content=""
+
+URL='https://www.bsp-auto.com/fr/list.asp?ag_depart=2715&date_a='${TRIP_DATE_FROM_ENCODED}'&heure_a=15%3A00&ag_retour=2715&date_d='${TRIP_DATE_TO_ENCODED}'&heure_d=15%3A00&chkage=1&age=25'
+
+
+if [ $QUIET -eq 1 ]; then
+  echo "From ${TRIP_DATE_FROM} to ${TRIP_DATE_TO}"
+  echo "URL: ${URL}"
+  echo "Result file: $dirname/bsp-auto"
+  echo ""
+fi
 
 curl -s "$URL" > $dirname/bsp-auto.curl
 
@@ -40,9 +84,9 @@ for i in ${!cars[@]} ; do
 done
 
 # Add colummn current if set
-if [ "$current_price" != "" ] ; then
+if [ "$CURRENT_PRICE" != "" ] ; then
   header="$header,Current"
-  content="$content,$current_price"
+  content="$content,$CURRENT_PRICE"
 fi
 
 # Print intermediate result
@@ -61,7 +105,7 @@ for((i=0;i<${#line_last[@]};i++)) ; do
   # Handle new values
   if [ "${line_last[$i]}" != "" ] && [ "${line_before[$i]}" != "" ] ; then
     # Test if value is better than the min
-    if [ "${line_last[$i]}" -lt "${current_price}" ] ; then
+    if [ "${line_last[$i]}" -lt "${CURRENT_PRICE}" ] ; then
       flag_email=0
     fi
   fi
